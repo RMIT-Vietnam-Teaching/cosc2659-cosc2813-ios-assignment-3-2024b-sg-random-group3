6e0481e12cbc13b0e2284f36f9ff3bc996155b17
MinT <135308254+mintdunno@users.noreply.github.com>
2024-09-13 08:43:01 +0800

    - create f2learn app

---
+++ f2learn/f2learn/f2learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  f2learnApp.swift
+//  f2learn
+//
+//  Created by user264955 on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct f2learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
