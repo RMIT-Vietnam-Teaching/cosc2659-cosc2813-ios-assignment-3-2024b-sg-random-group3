c8d035abb64f8b2c8a601fefe0f1610f8eaf6c77
MinT <135308254+mintdunno@users.noreply.github.com>
2024-09-18 13:16:02 +0800

    remake model

---
+++ F2Learn/F2Learn/Model/AdminDashBoardModel.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+struct Stat: Identifiable {
+    let id = UUID()
+    let title: String
+    let value: String
+    let change: String?
+}
+
+struct QuickAction: Identifiable {
+    let id = UUID()
+    let title: String
+    let icon: String
+}
+
+struct RecentActivity: Identifiable {
+    let id = UUID()
+    let icon: String
+    let title: String
+    let description: String
+    let time: String
+}
+
+extension Date {
+    func timeAgoDisplay() -> String {
+        let formatter = RelativeDateTimeFormatter()
+        formatter.unitsStyle = .full
+        return formatter.localizedString(for: self, relativeTo: Date())
+    }
+}
