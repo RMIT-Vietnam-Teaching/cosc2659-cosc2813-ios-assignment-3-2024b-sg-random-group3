c3b4979b61567d99bc536a11079308f0e17aab85
Dutuanvu <s3924489@rmit.edu.vn>
2024-09-22 21:45:25 +0700

    add header

--- F2Learn/F2Learn/View/Extensions/ColorScheme.swift
+++ F2Learn/F2Learn/View/Extensions/ColorScheme.swift
@@ -1,3 +1,18 @@
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2023B
+  Assessment: Assignment 3
+  Author: Random Group 3/F2 Learn
+  ID: Tran Ngoc Minh – s3911737
+      Nguyen Duong Truong Thinh – s3914412
+      Dang Minh Triet – s4022878
+      Du Tuan Vu – s3924489
+  Created  date:  26/08/2024
+  Last modified:  23/09/2024
+  Acknowledgement: RMIT Canvas( tutorials, modules)
+*/
+
 //import SwiftUI
 //
 //extension Color {
--- F2Learn/F2LearnUITests/F2LearnUITests.swift
+++ F2Learn/F2LearnUITests/F2LearnUITests.swift
@@ -1,9 +1,17 @@
-//
-//  F2LearnUITests.swift
-//  F2LearnUITests
-//
-//  Created by user264955 on 9/16/24.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2023B
+  Assessment: Assignment 3
+  Author: Random Group 3/F2 Learn
+  ID: Tran Ngoc Minh – s3911737
+      Nguyen Duong Truong Thinh – s3914412
+      Dang Minh Triet – s4022878
+      Du Tuan Vu – s3924489
+  Created  date:  26/08/2024
+  Last modified:  23/09/2024
+  Acknowledgement: RMIT Canvas( tutorials, modules)
+*/
 
 import XCTest
 
